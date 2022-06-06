1595d34ba9837009bfd42f390e123fe0ef355d02
Adamaristama <53483448+Adamaristama@users.noreply.github.com>
2022-06-06 20:13:03 +0700

    Add files via upload

---
+++ Backend/flask/imgpreprocess.py
@@ -0,0 +1,18 @@
+import json
+import tensorflow as tf
+import os
+import numpy as np
+import base64
+from PIL import Image
+import io
+
+
+def preproc(data):
+    imgdata = base64.decodebytes(data)
+    image = Image.open(io.BytesIO(imgdata)).convert('RGB')
+    image_new = image.resize((224, 224))
+    x = np.array(image_new)
+    x = np.expand_dims(x, axis=0)
+    x = x / 255.0
+    new_x = np.vstack([x])
+    return 
---
+++ Backend/flask/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+import numpy as np
+import os
+import json
+from predict import do_predict
+from imgpreprocess import preproc
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    data = {'status': 200, 'data': 'Hello there'}
+    return json.dumps(data)
+
+
+@app.route('/api/predict/', methods=['POST'])
+def predict_img():
+    item = request.json
+    item_dict = json.loads(item)
+    jsonResponse = do_predict(item_dict['data'])
+    return jsonResponse
+
+
+@app.route('/api/test/', methods=['POST'])
+def test_post():
+    item = request.json
+    jsonResult = {
+        'data': item,
+    }
+    return json.dumps(jsonResult)
+
+
+@app.route('/api/predict/img/', methods=['POST'])
+def predict_full_img():
+    item = request.json
+    item_dict = json.loads(item, strict=False)
+    encodedItem = item_dict['data'].encode('utf8')
+    img_data = preproc(encodedItem)
+    jsonResponse = do_predict(img_data)
+    return jsonResponse
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
